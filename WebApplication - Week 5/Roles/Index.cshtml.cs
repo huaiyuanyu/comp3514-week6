9b29a1dd01127859a5fb2f9976822328d968ccf2
Gary Yu <hyu100@my.bcit.ca>
2023-10-04 15:23:40 -0700

    alter role page to include IDs

--- WebApplication - Week 5/Roles/Index.cshtml.cs
+++ WebApplication - Week 5/Roles/Index.cshtml.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using WebApplication___Week_5.Data;
+using WebApplication___Week_5.Repositories;
 using WebApplication___Week_5.ViewModels;
 
 namespace WebApplication___Week_5.Roles
@@ -13,20 +15,20 @@ namespace WebApplication___Week_5.Roles
     public class IndexModel : PageModel
     {
         private readonly WebApplication___Week_5.Data.ApplicationDbContext _context;
+        RoleRepo _roleRepo = null;
 
         public IndexModel(WebApplication___Week_5.Data.ApplicationDbContext context)
         {
             _context = context;
+            _roleRepo = new RoleRepo(context);
         }
 
-        public IList<RoleVM> RoleVM { get;set; } = default!;
+        public List<RoleVM> RoleVM { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
-            if (_context.RoleVM != null)
-            {
-                RoleVM = await _context.RoleVM.ToListAsync();
-            }
+            RoleVM = await _roleRepo.GetAll();
         }
     }
+
 }
